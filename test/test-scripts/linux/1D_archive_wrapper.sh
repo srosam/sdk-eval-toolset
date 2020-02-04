89c8b9321beac542e467410f1f4032a759b3d477
Safian Khan <skhan@gw.local>
2020-02-04 14:58:59 +0000

    Commit of linux Archive Wrapper script used for testing along with relevant libraries and config files.

---
+++ test/test-scripts/linux/1D_archive_wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# CALL ARCHIVE WRAPPER AND PROCESS ARCHIVES
+echo "Archive wrapper"
+
+# DISPLAY INPUT FILES
+echo "Display input files"
+ls "../../test-data/1D-Archives"
+
+# USE GLASSWALL CLASSIC TO CALL ARCHIVE WRAPPER AND PROCESS ARCHIVES
+echo "Calls Archive Wrapper to process Archives"
+cd "../../../libraries/linux"
+./glasswallCLI -config="../../test/test-scripts/linux/config.ini" -xmlconfig="../../test/test-scripts/linux/config.xml"
+read
+
+# --- List all the files in the output directory
+echo "All output files"
+ls "../../test/test-output/1D_Archive"
+
+# --- Show ouput directory
+echo "Open the output folder"
+xdg-open "../../test/test-output/1D_Archive"
+read 
