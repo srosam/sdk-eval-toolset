25a51335cca82227d2df21d575445ecf890b8ffa
Paul Gerard <paulfgerard@hotmail.com>
2020-02-21 15:35:24 +0000

    Update Docker Run command to avoid having to navigate to working folder from command line By using the -w argument we can avoid the user having to navigate to the required folder before running the next script.

--- test/os-tests/UBUNTU_19/run.bat
+++ test/os-tests/UBUNTU_19/run.bat
@@ -1,3 +1,3 @@
 docker build -t python_wrapper_ubuntu:19.04 .
-docker run -it -v %cd%\input:/home/glasswall/input -v %cd%\output:/home/glasswall/output python_wrapper_ubuntu:19.04
+docker run -it -v %cd%\input:/home/glasswall/input -v %cd%\output:/home/glasswall/output -w="/home/glasswall" python_wrapper_ubuntu:19.04
 PAUSE
