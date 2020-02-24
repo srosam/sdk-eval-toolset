987a5509ad57b22bbdb2b5fbd19c77c378d3cae0
Matthew-Alaric <57911802+Matthew-Alaric@users.noreply.github.com>
2020-02-24 11:03:25 +0000

    Update run.sh

--- test/os-tests/UBUNTU_19/run.sh
+++ test/os-tests/UBUNTU_19/run.sh
@@ -1,3 +1,3 @@
 cd Python_Wrapper
-python PythonWrapperTestApp.py -w Python_Wrapper/Glasswall.py -i ../input -o ../output -c Config.xml -d ../libraries/libglasswall_core2.so
+python3 PythonWrapperTestApp.py -w Python_Wrapper/Glasswall.py -i ../input -o ../output -c Config.xml -d ../libraries/libglasswall_core2.so
 
